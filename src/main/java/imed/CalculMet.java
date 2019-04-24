9204b9e33be2fdcfe85759d559f47fd563d9494b
Aymen Braghtha <aymenbraghtha@yahoo.fr>
2019-04-24 14:20:36 +0200

    aymen

---
+++ src/main/java/imed/CalculMet.java
@@ -0,0 +1,23 @@
+package imed;
+
+public class CalculMet {
+	
+	public double somme(double a,double b) {
+		
+		return a+b;
+	}
+public double produit(double a,double b) {
+		
+		return a*b;
+	}
+
+public double soustract(double a,double b) {
+		
+		return a-b;
+	}
+public double divis(double a,double b) {
+		
+		return a/b;
+	}	
+}
+
