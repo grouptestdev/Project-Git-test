303ef9e3cfd20e5a324d9d6de762202e58d1f429
bassem <bassembelmabrouk@live.fr>
2019-04-24 15:37:56 +0100

    test

---
+++ src/main/java/imed/CalculMet.java
@@ -0,0 +1,34 @@
+package imed;
+
+public class CalculMet {
+	
+	public double somme(double a,double b) {
+		
+		return a+b;
+	}
+public double produit(double a,double b) {
+		
+		return a*b;
+	}
+
+public double soustract(double a,double b) {
+		
+		return a-b;
+		
+	}
+	
+	public double division(double a,double b) {
+		
+		return a/b;
+		
+		
+		
+		
+		
+	}
+		public String sayHello() {
+		
+		Syteme.out.println("hello");
+	
+	}
+}
