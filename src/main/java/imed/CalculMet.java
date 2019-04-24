e36829b6c9df1655bda99c42141f5b0ff50e56ae
Aymen Braghtha <aymenbraghtha@yahoo.fr>
2019-04-24 14:37:16 +0200

    aymen1111

--- src/main/java/imed/CalculMet.java
+++ src/main/java/imed/CalculMet.java
@@ -14,22 +14,12 @@ public double produit(double a,double b) {
 public double soustract(double a,double b) {
 		
 		return a-b;
-<<<<<<< HEAD
-	}
-public double divis(double a,double b) {
-		
-		return a/b;
-	}	
-}
-
-=======
 		
 	}
 	
-	public double div(double a,double b) {
+	public double division(double a,double b) {
 		
 		return a/b;
 		
 	}
 }
->>>>>>> 15858032c7caa5fa74e29a488ed12b64a7f08baa
